aea389a54e306a40e75bbabc2be205bd3d2e8d4b
Ilkin Valiyev <ilkinv1994@gmail.com>
2021-01-02 22:37:53 +0400

    ilk kodlarin yazilmasi

---
+++ lib/burc_detail.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BurcDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/burc_lists.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BurcLists extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Burc Rehberi", style: TextStyle(fontSize: 22),textAlign: TextAlign.center,),
+      ),
+    );
+  }
+}
